3dec96d7a830ec679c016b88da592f62f77d8dda
Ahmed Nawar <153510555+AhmedNawar2003@users.noreply.github.com>
2025-02-10 22:08:22 +0200

    changes

--- src/app/admin/articles-table/edit/[id]/page.tsx
+++ src/app/admin/articles-table/edit/[id]/page.tsx
@@ -5,47 +5,22 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import EditArticleForm from "./EditArticleForm";
 
-
-
 const EditArticlePage = async ({ params }: { params: { id: string } }) => {
-  // Ensure the parameter is correctly extracted
-  if (!params?.id) {
-    redirect("/");
-  }
-
-  const cookieStore = await cookies();
-  const token = cookieStore.get("jwtToken")?.value;
-
-  if (!token) {
-    redirect("/");
-  }
-
+  const token = (await cookies()).get("jwtToken")?.value;
+  if (!token) redirect("/");
   const payload = verifyTokenForPage(token);
-  if (!payload?.isAdmin) {
-    redirect("/");
-  }
-
-  try {
-    const article: Article | null = await getSingleArticle(params.id);
-
-    if (!article) {
-      redirect("/not-found");
-    }
-
-    return (
-      <section className="fix-height flex items-center justify-center px-5 lg:px-20">
-        <div className="shadow p-4 bg-purple-200 rounded w-full">
-          <h2 className="text-2xl text-green-700 font-semibold mb-4">
-            Edit Article
-          </h2>
-          <EditArticleForm article={article} />
-        </div>
-      </section>
-    );
-  } catch (error) {
-    console.error("Error fetching article:", error);
-    redirect("/error");
-  }
+  if (!payload?.isAdmin) redirect("/");
+  const article: Article = await getSingleArticle(params.id);
+  return (
+    <section className="fix-height flex items-center justify-center px-5 lg:px-20">
+      <div className="shadow p-4 bg-purple-200 rounded w-full">
+        <h2 className="text-2xl text-green-700 font-semibold mb-4">
+          Edit Article
+        </h2>
+        <EditArticleForm article={article} />
+      </div>
+    </section>
+  );
 };
 
 export default EditArticlePage;
